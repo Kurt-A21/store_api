9011819d9ba6106da15c5fe0d8f6ed54b261cc5e
Kurt Adriaanse <93592151+Kurt-A21@users.noreply.github.com>
2024-07-08 11:52:12 +0200

    Added sample APIs

--- app.py
+++ app.py
@@ -1,6 +1,62 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+stores = [
+    {
+        "name": "My Store",
+        "items": [
+            {
+                "name": "chair",
+                "price": 20
+            },
+            {
+                "name": "desk",
+                "price": 30
+            }
+        ]
+    }
+]
+
+@app.get("/store")
+def get_stores():
+    return {"store": stores}
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "Store not found"}, 404
+         
+@app.get("/store/<string:name>/item")
+def get_item(name):
+    for store in stores:
+        if store["name"] == name:
+            return store["items"]
+    return {"message": "Store not found"}, 404
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404
+
+@app.delete("/store/<string:name>")
+def delete_store(name):
+    for store in stores:
+        if store["name"] == name:
+            store.remove(name)
+            return {"message": "Store was deleted"}, 200
+    return {"message": "Store not found"}, 404
